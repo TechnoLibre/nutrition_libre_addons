eb26c9606e918fc293ff3f07875888f996a6333b
Mathieu Benoit <mathben@technolibre.ca>
2024-08-18 10:01:44 -0400

    [ADD] module to update clienta 1

---
+++ cg_migrate_data_hook_clienta_update_1/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Migrate sqlserver for ClientA update 1",
+    "version": "16.0.0.1",
+    "author": "TechnoLibre",
+    "website": "https://technolibre.ca",
+    "license": "AGPL-3",
+    "category": "Extra tools",
+    "summary": "Migrate database of project clienta update 1",
+    "description": """
+Migrate sqlserver for ClientA Update 1
+======================================
+""",
+    "depends": [],
+    "post_init_hook": "post_init_hook",
+    "installable": True,
+}
---
+++ cg_migrate_data_hook_clienta_update_1/hooks.py
@@ -0,0 +1,52 @@
+import base64
+import datetime
+
+# import locale
+import logging
+from collections import defaultdict
+
+from odoo import SUPERUSER_ID, _, api, fields, models
+from odoo.exceptions import ValidationError
+
+_logger = logging.getLogger(__name__)
+
+
+def post_init_hook(cr, e):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    _logger.info("Start update 1")
+
+    # Fix all formation without user, but they buy it
+    slide_channel_ids = env["slide.channel"].search([("active", "=", False)])
+    for slide_channel_id in slide_channel_ids:
+        product_id = slide_channel_id.product_id
+        sale_order_line_ids = env["sale.order.line"].search(
+            [("product_id", "=", product_id.id)]
+        )
+        for sale_order_line_id in sale_order_line_ids:
+            # partner_ids = env["res.partner"].browse(
+            #     [a.order_partner_id.id for a in sale_order_line_ids]
+            # )
+            partner_id = sale_order_line_id.order_partner_id
+            value_slide_channel_partner = {
+                "channel_id": slide_channel_id.id,
+                "completion": 0,
+                "completed_slides_count": 0,
+                "completed": False,
+                "partner_id": partner_id.id,
+                "create_date": sale_order_line_id.create_date,
+            }
+            # Validate if exist
+            obj_slide_channel_partner = env[
+                "slide.channel.partner"
+            ].search(
+                [
+                    ("partner_id", "=", partner_id.id),
+                    ("channel_id", "=", slide_channel_id.id),
+                ],
+                limit=1,
+            )
+            if not obj_slide_channel_partner:
+                obj_slide_channel_partner = env[
+                    "slide.channel.partner"
+                ].create(value_slide_channel_partner)
+
