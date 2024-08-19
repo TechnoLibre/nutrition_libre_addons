9fb32e21eef4ac36e5ce074bbb5cf4175b9b483e
Mathieu Benoit <mathben@technolibre.ca>
2024-08-19 00:36:24 -0400

    up

--- cg_migrate_data_hook_clienta_update_1/hooks.py
+++ cg_migrate_data_hook_clienta_update_1/hooks.py
@@ -2,6 +2,7 @@ import base64
 import datetime
 
 # import locale
+import re
 import logging
 from collections import defaultdict
 
@@ -9,6 +10,7 @@ from odoo import SUPERUSER_ID, _, api, fields, models
 from odoo.exceptions import ValidationError
 
 _logger = logging.getLogger(__name__)
+LST_KEY_EVENT = []
 
 
 def post_init_hook(cr, e):
@@ -47,3 +49,32 @@ def post_init_hook(cr, e):
                 obj_slide_channel_partner = env[
                     "slide.channel.partner"
                 ].create(value_slide_channel_partner)
+
+    # Create event if not exist and update with all partner with invoice
+    # pattern_formation_no = r"\b\d+\.\w+\b"
+    # product_ids = env["product.template"].search(
+    #     [("detailed_type", "=", "course")]
+    # )
+    # for product_id in product_ids:
+    #     partner_id = env["res.partner"]
+    #     match = re.search(pattern_formation_no, product_id.name)
+    #     if match:
+    #         no_formation = match.group()
+    #         if no_formation in LST_KEY_EVENT:
+    #             sale_order_line_ids = env["sale.order.line"].search(
+    #                 [("product_id", "=", product_id.id)]
+    #             )
+    #             for sale_order_line_id in sale_order_line_ids:
+    #                 partner_id += sale_order_line_id.order_partner_id
+    #         if partner_id.exists():
+    #             # Search event, or create it
+    #             event_ids = env["event.event"].search([])
+    #             for event_id in event_ids:
+    #                 if no_formation.lower() in event_id.name.lower():
+    #                     break
+    #             else:
+    #                 # Not found
+    #                 value_event = {"name": f"Formation {no_formation}"}
+    #                 event_id = env["event.event"].create(value_event)
+    #             if event_id:
+    #                 print("ok")
