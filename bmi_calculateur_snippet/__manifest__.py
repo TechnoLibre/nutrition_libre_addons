a56498bec588807a0280339344c6315e3b9ed786
AdilGoumih <2333382@cegepmontpetit.ca>
2024-09-19 09:48:53 -0400

    Modification des fichiers init et manifest

---
+++ bmi_calculateur_snippet/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'BMI Calculateur Snippet',
+    "version": "16.0.1.0.1",
+    'summary': 'Calculateur de IMC et Percentile',
+    'description': """Ce bout de code ajoute une fonctionnalité à votre site web : un calculateur d'IMC et de percentiles pour enfants.""",
+    'category': 'Website',
+    'author': 'Adil',
+    'website': 'http://www.votre-website.com',
+    'depends': ['website'],  # Assure-toi que le module 'website' est installé
+    'data': [
+        'views/assets.xml',  # Chemin des assets pour le CSS et JS
+        'views/snippet_templates.xml',  # Fichier template pour les snippets
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            # Utilisation des chemins complets pour CSS et JS
+            'bmi_calculateur_snippet/static/src/css/snippet_style.css',
+            'bmi_calculateur_snippet/static/src/js/bmi_calculator.js',
+        ],
+    },
+    'installable': True,  # Ce champ doit être True
+    'application': False,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
